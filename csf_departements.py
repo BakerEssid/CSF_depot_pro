f710ba03ee4ad4c2371245cd6ad44fad7c48594f
BakerEssid <baker.essid98@gmail.com>
2026-06-19 18:40:43 +0200

    Version 1.0

---
+++ csf_departements.py
@@ -0,0 +1,14 @@
+def afficher_departements():
+    departements = [
+        "Conseil",
+        "Solutions",
+        "Formation"
+    ]
+
+    print("Départements CSF :")
+
+    for dep in departements:
+        print("- " + dep)
+
+
+afficher_departements()
